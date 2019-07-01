a5eef11f9ee8bb3c49fdf901fcf034bea4c70ddc
Mauro Velazquez <mauerv@Mauros-MacBook-Air.local>
2019-07-01 16:17:53 +0200

    Remove presale info

--- server.js
+++ server.js
@@ -1,16 +1,6 @@
 const express = require('express')
 const app = express()
-const subdomain = require('express-subdomain')
 const bodyParser = require('body-parser')
-
-const router = express.Router()
-
-router.get('/', function(req, res) {
-  let path = __dirname + '/token.html'
-  res.sendFile(path)
-})
-
-app.use(subdomain('token', router))
 
 app.use(express.static(__dirname))
 app.use(bodyParser.urlencoded())
@@ -19,6 +9,5 @@ app.get('/', function(req, res) {
   let path = __dirname + '/index.html'
   res.sendFile(path)
 });
-
 
 app.listen(process.env.PORT || 8080)
